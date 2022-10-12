fe1cbd9eb67be788a788204b145241a455cd9229
Fernando Goncalves <fernando.hmg@gmail.com>
2022-10-11 21:28:13 -0300

    Button component

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,7 +6,53 @@ export default {
   component: Text,
   args: {
     children: "Hello world!",
+    size: "md",
+  },
+  argTypes: {
+    size: {
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "inline-radio",
+      },
+    },
   },
 } as Meta<TextProps>;
 
 export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: "sm",
+  },
+};
+
+export const Medium: StoryObj<TextProps> = {
+  args: {
+    size: "md",
+  },
+};
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: "lg",
+  },
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: <p>That's a custom component</p>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
