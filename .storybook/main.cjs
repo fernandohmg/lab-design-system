ea94859839a12edbdeed769aacda145d09c867b1
Fernando Goncalves <fernando.hmg@gmail.com>
2022-10-15 14:22:25 -0300

    adding chromatic

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,10 +12,4 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === "PRODUCTION") {
-      config.base = "/lab-design-system";
-    }
-    return config;
-  },
 };
